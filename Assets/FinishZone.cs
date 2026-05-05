0f7d4e3bee62229a23e0343dc7d6bfb6cf513edd
Shuqi IM <Ssq1011929374@gmail.com>
2026-05-05 14:35:25 +0800

    Update FinishZone.cs

--- Assets/FinishZone.cs
+++ Assets/FinishZone.cs
@@ -5,6 +5,7 @@ public class FinishZone : MonoBehaviour
     //only trigger when the player enters the finish zone
     void OnTriggerEnter(Collider other)
     {
+
         // check if the collided object has the tag "Player"
         if (other.CompareTag("Player"))
         {
