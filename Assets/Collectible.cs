01e95a17e132beec2ba983a4816133b532fbe457
Shuqi IM <Ssq1011929374@gmail.com>
2026-05-18 22:38:10 +0800

    Week5

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -2,22 +2,36 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [Tooltip("Points awarded to player when this collectible is picked up")]
+    public int score = 1;
+
+    public int BeCollected()
+    {
+        // 1. inform GameManager that an item has been collected, so it can update the count and check for win condition
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.ItemCollected();
+        }
+
+        Destroy(gameObject);
+        return score;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        // check if the collided object has the tag "Player"
+        // Check if the collided object is the player
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("A physical collision has been detected! Collector name: " + collision.gameObject.name);
 
-
-            // call GameManager to update the score or collected items count
-            if (GameManager.instance != null)
+            // Link with the message script on the player to award points
+            var playerMessage = collision.gameObject.GetComponent<message>();
+            if (playerMessage != null)
             {
-                GameManager.instance.ItemCollected();
+                
             }
 
-            // item destroyed after collection
-            Destroy(gameObject);
+            BeCollected();
         }
     }
 }
