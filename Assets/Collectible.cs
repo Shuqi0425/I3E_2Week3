6756b309d2c17440dc3f4c27af5fa713191afbe5
Shuqi IM <Ssq1011929374@gmail.com>
2026-05-19 21:12:16 +0800

    Update collect UI(text & pic)

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -5,33 +5,19 @@ public class Collectible : MonoBehaviour
     [Tooltip("Points awarded to player when this collectible is picked up")]
     public int score = 1;
 
+    // only called by the player when they interact with this collectible
     public int BeCollected()
     {
-        // 1. inform GameManager that an item has been collected, so it can update the count and check for win condition
+        // 1. inform the GameManager that an item has been collected 
         if (GameManager.instance != null)
         {
             GameManager.instance.ItemCollected();
         }
 
+        // 2. item should destroy itself after being collected
         Destroy(gameObject);
+
+        // 3. return the score value of this collectible to the player so they can update their score UI
         return score;
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        // Check if the collided object is the player
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("A physical collision has been detected! Collector name: " + collision.gameObject.name);
-
-            // Link with the message script on the player to award points
-            var playerMessage = collision.gameObject.GetComponent<message>();
-            if (playerMessage != null)
-            {
-                
-            }
-
-            BeCollected();
-        }
     }
 }
