6ecb705f900297ad2c2915cd64f152322b039706
Shuqi IM <Ssq1011929374@gmail.com>
2026-05-26 16:04:38 +0800

    audio

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -5,19 +5,46 @@ public class Collectible : MonoBehaviour
     [Tooltip("Points awarded to player when this collectible is picked up")]
     public int score = 1;
 
+    private AudioSource collectibleAudio;
+
+    void Start()
+    {
+        collectibleAudio = GetComponent<AudioSource>();
+    }
+
+    public void PlayCollectibleSound()
+    {
+        if (collectibleAudio != null)
+        {
+            if (collectibleAudio.clip != null)
+            {
+                collectibleAudio.Play();
+                Destroy(gameObject, collectibleAudio.clip.length);
+            }
+            else
+            {
+                collectibleAudio.Play();
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            Debug.Log("No AudioSource component found on this collectible.");
+            Destroy(gameObject);
+        }
+    }
+
     // only called by the player when they interact with this collectible
     public int BeCollected()
     {
-        // 1. inform the GameManager that an item has been collected 
         if (GameManager.instance != null)
         {
             GameManager.instance.ItemCollected();
         }
 
-        // 2. item should destroy itself after being collected
-        Destroy(gameObject);
+        // play audio and stop after destroyed itself
+        PlayCollectibleSound();
 
-        // 3. return the score value of this collectible to the player so they can update their score UI
         return score;
     }
 }
